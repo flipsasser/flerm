87627c7b3fe425694678e9e55a5c6a2a459fb6dc
Flip Sasser <flip@x451.com>
2012-12-27 18:03:23 -0500

    Updated install.sh to work good

--- install.sh
+++ install.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
-git clone git@github.com:flipsasser/flim.git ~/.flim
+echo "Installing flim..."
+
+if [ -d ~/.flim ]
+then
+  echo "  removing old versions"
+  rm -rf ~/.flim
+fi
+
+git clone git://github.com/flipsasser/flim.git ~/.flim
 cd ~/.flim
 git submodule update --init
-ln -s vimrc ~/.vimrc
-ln -s vim/ ~/.vim
+
+if [ -e ~/.vimrc -o -h ~/.vimrc ]
+then
+	echo "  backing up ~/.vimrc to ~/.vimrc.old"
+  mv ~/.vimrc ~/.vimrc.old
+fi
+
+ln -s ~/.flim/vimrc ~/.vimrc
+
+if [ -d ~/.vim/ ]
+then
+	echo "  backing up ~/.vim to ~/.vim.old"
+  mv ~/.vim ~/.vim.old
+fi
+
+ln -s ~/.flim/vim/ ~/.vim
