bb87ba3ccc5541ca2a37e8f5f29dbdca03a31632
Flip Sasser <flip@inthebackforty.com>
2013-04-02 09:56:07 -0400

    Shallow submodule updates

--- install.sh
+++ install.sh
@@ -9,7 +9,15 @@ fi
 
 git clone git://github.com/flipsasser/flim.git ~/.flim
 cd ~/.flim
-git submodule update --init
+
+# Shallow clone Git submodules to save time and energy
+git submodule init
+for i in $(git submodule | sed -e 's/.* //'); do
+  spath=$(git config -f .gitmodules --get submodule.$i.path)
+  surl=$(git config -f .gitmodules --get submodule.$i.url)
+  git clone --depth 1 $surl $spath
+done
+git submodule update
 
 if [ -e ~/.vimrc -o -h ~/.vimrc ]
 then
