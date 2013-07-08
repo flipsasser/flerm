65ce04d9ef7fa9648f36c4ec7c875c19891a2d13
Flip Sasser <flip@inthebackforty.com>
2013-07-08 15:18:55 -0400

    Fixed install.sh like a smart person undoing a stupid person's stupid actions

--- install.sh
+++ install.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 echo "Installing flim..."
 
-#if [ -d ~/.flim ]
-#then
-  #echo "  removing old versions"
-  #rm -rf ~/.flim
-#fi
+if [ -d ~/.flim ]
+then
+  echo "  removing old versions"
+  rm -rf ~/.flim
+fi
 
-#git clone git://github.com/flipsasser/flim.git ~/.flim
-#cd ~/.flim
+git clone git://github.com/flipsasser/flim.git ~/.flim
+cd ~/.flim
 
 git submodule init
 git submodule update
 
-#if [ -e ~/.vimrc -o -h ~/.vimrc ]
-#then
-	#echo "  backing up ~/.vimrc to ~/.vimrc.old"
-  #mv ~/.vimrc ~/.vimrc.old
-#fi
+if [ -e ~/.vimrc -o -h ~/.vimrc ]
+then
+        echo "  backing up ~/.vimrc to ~/.vimrc.old"
+  mv ~/.vimrc ~/.vimrc.old
+fi
 
-#ln -s ~/.flim/vimrc ~/.vimrc
+ln -s ~/.flim/vimrc ~/.vimrc
 
-#if [ -d ~/.vim/ ]
-#then
-	#echo "  backing up ~/.vim to ~/.vim.old"
-  #mv ~/.vim ~/.vim.old
-#fi
+if [ -d ~/.vim/ ]
+then
+        echo "  backing up ~/.vim to ~/.vim.old"
+  mv ~/.vim ~/.vim.old
+fi
 
-#ln -s ~/.flim/ ~/.vim
+ln -s ~/.flim/ ~/.vim
 
-#if [ -e ~/.profile -o -h ~/.profile ]
-#then
-	#echo "  backing up ~/.profile to ~/.profile.old"
-  #mv ~/.profile ~/.profile.old
-#fi
+if [ -e ~/.profile -o -h ~/.profile ]
+then
+        echo "  backing up ~/.profile to ~/.profile.old"
+  mv ~/.profile ~/.profile.old
+fi
 
-#ln -s ~/.flim/profile ~/.profile
+ln -s ~/.flim/profile ~/.profile
